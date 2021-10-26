b5992044ffeb7c574c68e68bf1a3357537f776f0
Chris <christianbauer92@gmail.com>
2021-10-26 20:40:01 +0200

    Add thread control mixin

---
+++ src/thread_extension/control.py
@@ -0,0 +1,74 @@
+# pylint: disable=missing-function-docstring
+"""
+Thread-control extensions.
+"""
+from threading import Event
+from typing import Optional
+
+
+class ThreadControlMixin:
+    """
+    This class extends a thread of control by providing events to pause and
+    stop a thread at runtime.
+    """
+    def __init__(self) -> None:
+        self.__started = Event()
+        self.__stopped = Event()
+        self.__running = Event()
+
+    def __repr__(self) -> str:
+        status = "running" if self.is_running else "paused"
+        if not self.__started.is_set():
+            status = "initial"
+        if self.is_stopped:
+            status = "stopped"
+        return status
+
+    @property
+    def is_running(self) -> bool:
+        return self.__running.is_set()
+
+    @property
+    def is_stopped(self) -> bool:
+        return self.__stopped.is_set()
+
+    def pause(self) -> None:
+        if not self.__started.is_set():
+            raise RuntimeError("Cannot pause thread before it is started")
+        if self.is_stopped:
+            raise RuntimeError("Cannot pause thread after it is stopped")
+        self.__running.clear()
+
+    def resume(self) -> None:
+        if not self.__started.is_set():
+            raise RuntimeError("Cannot resume thread before it is started")
+        if self.is_stopped:
+            raise RuntimeError("Cannot resume thread after it is stopped")
+        self.__running.set()
+
+    def stop(self) -> None:
+        if not self.__started.is_set():
+            raise RuntimeError("Cannot stop thread before it is started")
+        if self.is_stopped:
+            raise RuntimeError("thread can only be stopped once")
+        self.__stopped.set()
+        if not self.is_running:
+            # Release lock by setting event flag
+            self.__running.set()
+
+    def wait(self, timeout: Optional[float] = None) -> bool:
+        if not self.__started.is_set():
+            raise RuntimeError("Cannot wait thread before it is started")
+        if self.is_stopped:
+            raise RuntimeError("Cannot wait thread after it is stopped")
+        return self.__running.wait(timeout=timeout)
+
+    def set_start_state(self) -> None:
+        if self.__started.is_set():
+            raise RuntimeError("thread can only be started once")
+        self.__started.set()
+        self.__running.set()
+
+    def set_end_state(self) -> None:
+        if self.is_stopped:
+            self.__running.clear()
---
+++ test/thread_extension/test_control.py
@@ -0,0 +1,112 @@
+import unittest
+from timeit import default_timer as timer
+from src.thread_extension import control
+
+
+class ThreadControlMixinClass(unittest.TestCase):
+    """
+    This class represents a wrapper class for all unittests related to the
+    ThreadControlMixin class within <src.thread_extension.control>.
+    """
+    def setUp(self):
+        self._control = control.ThreadControlMixin()
+
+    def tearDown(self):
+        del self._control
+
+    def test_additional_thread_control_states(self):
+        """
+        This test checks the correct transition into all possible states.
+        """
+        self.assertFalse(self._control.is_running)
+        self.assertFalse(self._control.is_stopped)
+        self.assertEqual(repr(self._control), "initial")
+        self._control.set_start_state()
+        self.assertTrue(self._control.is_running)
+        self.assertFalse(self._control.is_stopped)
+        self.assertEqual(repr(self._control), "running")
+        self._control.pause()
+        self.assertFalse(self._control.is_running)
+        self.assertFalse(self._control.is_stopped)
+        self.assertEqual(repr(self._control), "paused")
+        self._control.resume()
+        self.assertTrue(self._control.is_running)
+        self.assertFalse(self._control.is_stopped)
+        self.assertEqual(repr(self._control), "running")
+        self._control.pause()
+        self._control.stop()
+        self.assertTrue(self._control.is_running)
+        self.assertTrue(self._control.is_stopped)
+        self.assertEqual(repr(self._control), "stopped")
+        self._control.set_end_state()
+        self.assertFalse(self._control.is_running)
+        self.assertTrue(self._control.is_stopped)
+        self.assertEqual(repr(self._control), "stopped")
+
+    def test_paused_thread_timeout(self):
+        """
+        The test checks if a paused thread is released once the timeout occurs.
+        """
+        self._control.set_start_state()
+        self._control.pause()
+        start = timer()
+        self._control.wait(timeout=1)
+        end = timer()
+        self.assertTrue(1.0 <= (end - start) < 1.1)
+
+    def test_pre_start_exceptions(self):
+        """
+        This test checks all invalid calls before the thread is started.
+        """
+        with self.assertRaises(RuntimeError) as context:
+            self._control.pause()
+        exception = "Cannot pause thread before it is started"
+        self.assertTrue(exception in str(context.exception))
+
+        with self.assertRaises(RuntimeError) as context:
+            self._control.resume()
+        exception = "Cannot resume thread before it is started"
+        self.assertTrue(exception in str(context.exception))
+
+        with self.assertRaises(RuntimeError) as context:
+            self._control.stop()
+        exception = "Cannot stop thread before it is started"
+        self.assertTrue(exception in str(context.exception))
+
+        with self.assertRaises(RuntimeError) as context:
+            self._control.wait(timeout=1)
+        exception = "Cannot wait thread before it is started"
+        self.assertTrue(exception in str(context.exception))
+
+    def test_post_start_exceptions(self):
+        """
+        This test checks all invalid calls after the thread is stared.
+        """
+        self._control.set_start_state()
+
+        with self.assertRaises(RuntimeError) as context:
+            self._control.set_start_state()
+        exception = "thread can only be started once"
+        self.assertTrue(exception in str(context.exception))
+
+        self._control.stop()
+
+        with self.assertRaises(RuntimeError) as context:
+            self._control.pause()
+        exception = "Cannot pause thread after it is stopped"
+        self.assertTrue(exception in str(context.exception))
+
+        with self.assertRaises(RuntimeError) as context:
+            self._control.resume()
+        exception = "Cannot resume thread after it is stopped"
+        self.assertTrue(exception in str(context.exception))
+
+        with self.assertRaises(RuntimeError) as context:
+            self._control.wait(timeout=1)
+        exception = "Cannot wait thread after it is stopped"
+        self.assertTrue(exception in str(context.exception))
+
+        with self.assertRaises(RuntimeError) as context:
+            self._control.stop()
+        exception = "thread can only be stopped once"
+        self.assertTrue(exception in str(context.exception))
