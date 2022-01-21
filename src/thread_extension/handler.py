0327305f2e3e4c9725f6899ca11026e8023963ef
Chris <christianbauer92@gmail.com>
2022-01-21 16:57:16 +0100

    Add initial version of CycleWorkerThread

---
+++ src/thread_extension/handler.py
@@ -0,0 +1,127 @@
+# pylint: disable=missing-function-docstring
+"""
+Thread based handlers.
+"""
+import time
+from threading import Thread, Event
+from typing import Callable, Optional
+from src.thread_extension.control import ThreadControlMixin
+
+
+class CycleWorkerThread(Thread):
+    """
+    This class represents a special thread type, which executes a predefined
+    routine cyclically until a stop event is triggered.
+    """
+    def __init__(self,
+                 delay: float = 0.0,
+                 timeout: float = 1000.0,
+                 target: Optional[Callable] = None,
+                 args: tuple = (),
+                 kwargs=None,
+                 daemon: Optional[bool] = False
+                 ) -> None:
+        """
+        Initializes CycleWorkerThread class.
+        """
+        super().__init__(daemon=daemon)
+        self._control = ThreadControlMixin()
+        self._timeout = timeout
+        self._delay = delay
+        self._target = target
+        self._args = args
+        self._kwargs = kwargs if kwargs is not None else {}
+        self._task_done = Event()
+        self._task_done.set()
+
+    def __repr__(self) -> str:
+        string = super().__repr__()
+        if self.is_alive():
+            string = string[:-2] + f", {repr(self._control)})>"
+        return string
+
+    def run(self) -> None:
+        """
+        Defines the workers's concrete workflow.
+        """
+        self._control.set_start_state()
+        try:
+            self.preparation()
+            while not self._control.is_stopped():
+                if not self._control.wait(self._timeout):
+                    self.stop()  # Force stoppage
+                    break
+                self._task_done.clear()
+                try:
+                    self.work_routine()
+                finally:
+                    self._task_done.set()
+                time.sleep(self._delay)
+            self.post_processing()
+        finally:
+            self._control.set_end_state()
+            # Avoid a refcycle if the thread is running a function with
+            # an argument that has a member that points to the thread.
+            del self._target, self._args, self._kwargs
+
+    def work_routine(self) -> None:
+        """
+        Representing the worker's activity on each cycle.
+
+        You may override this method in a subclass. The work_routine() method
+        invokes the callable object passed to the object's constructor as the
+        target argument, if any, with sequential and keyword arguments taken
+        from the args and kwargs arguments, respectively.
+        """
+        if self._target:
+            self._target(*self._args, **self._kwargs)
+
+    def is_working(self):
+        return not self._task_done.is_set()
+
+    def pause(self) -> None:
+        self._control.pause()
+
+    def resume(self) -> None:
+        self._control.resume()
+
+    def stop(self) -> None:
+        self._control.stop()
+
+    @property
+    def delay(self) -> float:
+        """
+        Indicates how much time shall pass before the worker continues with
+        the next work cycle.
+        """
+        return self._delay
+
+    @delay.setter
+    def delay(self, delay: float) -> None:
+        if delay < 0.0:
+            raise ValueError("Delay must be non-negative")
+        self._delay = delay
+
+    @property
+    def timeout(self) -> float:
+        """
+        Indicates how much time the worker is allowed to pause before
+        the worker is automatically forced to stop.
+        """
+        return self._timeout
+
+    @timeout.setter
+    def timeout(self, timeout: float) -> None:
+        if timeout < 0.0:
+            raise ValueError("Timeout must be non-negative")
+        self._timeout = timeout
+
+    def preparation(self) -> None:
+        """
+        Optional preparatory steps for the worker to perform before starting.
+        """
+
+    def post_processing(self) -> None:
+        """
+        Optional follow-up steps for the worker to perform after stoppage.
+        """
---
+++ test/thread_extension/test_handler.py
@@ -0,0 +1,128 @@
+import time
+import unittest
+from src.thread_extension import handler
+
+
+class CycleWorkerThreadClass(unittest.TestCase):
+    """
+    This class represents a wrapper class for all unittests related to the
+    CycleWorkerThread class within <src.thread_extension.handler>.
+    """
+    @staticmethod
+    def work_routine() -> None:
+        """
+        Simulating a specific worker, that needs 100 ms to finish it's
+        work routine.
+        """
+        time.sleep(0.1)
+
+    def setUp(self):
+        self.__worker = handler.CycleWorkerThread(target=self.work_routine)
+
+    def tearDown(self):
+        del self.__worker
+
+    def test_property_delay(self):
+        """
+        This test checks if the property delay is set correctly.
+        """
+        self.assertEqual(self.__worker.delay, 0.0)
+        self.__worker.delay = 1.0
+        self.assertEqual(self.__worker.delay, 1.0)
+        with self.assertRaises(ValueError) as context:
+            self.__worker.delay = -1.0
+        self.assertTrue("Delay must be non-negative" in str(context.exception))
+
+    def test_property_timeout(self):
+        """
+        This test checks if the property timeout is set correctly.
+        """
+        self.assertEqual(self.__worker.timeout, 1000.0)
+        self.__worker.timeout = 500.0
+        self.assertEqual(self.__worker.timeout, 500.0)
+        with self.assertRaises(ValueError) as context:
+            self.__worker.timeout = -500.0
+        self.assertTrue("Timeout must be non-negative" in str(context.exception))
+
+    def test_start_pause_resume_stop(self):
+        """
+        This test checks if an active worker can transition into all states.
+        """
+        self._verify_initial_state()
+
+        self.__worker.start()
+        self._verify_operative_state()
+
+        self.__worker.pause()
+        self._verify_pause_state()
+
+        self.__worker.resume()
+        self._verify_operative_state()
+
+        self.__worker.stop()
+        self._verify_stopped_state()
+
+    def test_start_pause_stop(self):
+        """
+        This test checks if an active worker can be forced to stop working
+        permanently during pause.
+        """
+        self.__worker.timeout = 1.0
+        self._verify_initial_state()
+
+        self.__worker.start()
+        self._verify_operative_state()
+
+        self.__worker.pause()
+        self._verify_pause_state()
+
+        self.__worker.stop()
+        self._verify_stopped_state()
+
+    def test_start_pause_timeout(self):
+        """
+        This test checks if an active worker automatically stops working
+        permanently once the maximum pause time passed.
+        """
+        self.__worker.timeout = 1.0
+        self._verify_initial_state()
+
+        self.__worker.start()
+        self._verify_operative_state()
+
+        self.__worker.pause()
+        self._verify_stopped_state()
+
+    def _verify_initial_state(self):
+        self.assertFalse(self.__worker.is_alive())
+        self.assertFalse(self.__worker.is_working())
+        self.assertFalse(self.__worker._control.is_running())
+        self.assertFalse(self.__worker._control.is_stopped())
+        self.assertIn("initial", repr(self.__worker))
+
+    def _verify_operative_state(self):
+        self.assertTrue(self.__worker.is_alive())
+        self.assertTrue(self.__worker._control.is_running())
+        self.assertFalse(self.__worker._control.is_stopped())
+        self.assertIn("started", repr(self.__worker))
+        self.assertIn("running", repr(self.__worker))
+
+    def _verify_pause_state(self):
+        while self.__worker.is_working():
+            time.sleep(0.005)
+        self.assertTrue(self.__worker.is_alive())
+        self.assertFalse(self.__worker.is_working())
+        self.assertFalse(self.__worker._control.is_running())
+        self.assertFalse(self.__worker._control.is_stopped())
+        self.assertIn("started", repr(self.__worker))
+        self.assertIn("paused", repr(self.__worker))
+
+    def _verify_stopped_state(self):
+        while self.__worker.is_working():
+            time.sleep(0.005)
+        self.__worker.join(timeout=2.0)
+        self.assertFalse(self.__worker.is_alive())
+        self.assertFalse(self.__worker.is_working())
+        self.assertFalse(self.__worker._control.is_running())
+        self.assertTrue(self.__worker._control.is_stopped())
+        self.assertIn("stopped", repr(self.__worker))
