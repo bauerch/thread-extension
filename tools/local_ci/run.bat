5b7733e01f3d0f98910d73acf1f202eb3bf6cfb3
bauerch <christianbauer92@googlemail.com>
2022-02-11 23:51:26 +0100

    Rename test to tests in CI

--- tools/local_ci/run.bat
+++ tools/local_ci/run.bat
@@ -5,7 +5,7 @@ rem Activate conda virtual environment
 call activate .\venv\
 
 echo #### 1. RUN UNIT TESTS ####################################################
-python -m coverage run --rcfile=.coveragerc -m unittest discover -s test -p "test_*.py" -b
+python -m coverage run --rcfile=.coveragerc -m unittest discover -s tests -p "test_*.py" -b
 python -m coverage report --rcfile=.coveragerc -m
 del .coverage
 
