ca1ac77edc58ee6e3122105958e62596e039e0c9
bauerch <christianbauer92@googlemail.com>
2022-04-08 21:29:41 +0200

    Add editor config and setup files

--- src/worker_threads/version.py
+++ src/worker_threads/version.py
@@ -1,5 +1,5 @@
 """
-Contains the current version of thread-extension which can be used in setup.py
+Contains the current version of worker-threads which can be used in setup.py
 and can also be used to determine the version during runtime.
 """
 
